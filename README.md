2078af3ca050b5dced13133efc3af0939f078081
Rodrigo Afonso dos Santos <rasantos@protonmail.com>
2023-08-18 22:28:46 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do conteúdo abordado no https://curso.dev, desenvolvendo um projeto de ponta à ponta.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <img src="https://i.pinimg.com/originals/ff/a2/de/ffa2de19179d718879a1317ceb3f8465.jpg"/>
+  return (
+    <img src="https://i.pinimg.com/originals/ff/a2/de/ffa2de19179d718879a1317ceb3f8465.jpg" />
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
